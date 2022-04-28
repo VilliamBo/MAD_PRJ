c3638a119a78f51eb374435cffe5f41204243aac
Biavleren <71124726+Biavleren@users.noreply.github.com>
2022-04-28 18:58:16 +0200

    gradle update + hangoutActivity btn added

--- HangOutz/app/src/main/java/dk/au/mad22spring/AppProject/Group13/LoginActivity.java
+++ HangOutz/app/src/main/java/dk/au/mad22spring/AppProject/Group13/LoginActivity.java
@@ -2,7 +2,9 @@ package dk.au.mad22spring.AppProject.Group13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -10,13 +12,46 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
-    private Button loginBtn;
+    private Button loginBtn, newUserBtn;
+
+    //this should be deleted
+    private Button goToHangBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        
+
+        setupWidgets();
+
+    }
+
+    private void setupWidgets() {
+        loginBtn = findViewById(R.id.loginBtn);
+        newUserBtn = findViewById(R.id.newUserBtn);
+
+        goToHangBtn = findViewById(R.id.goToHangOutzBtn);
+        goToHangBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, HangOutzActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOn();
+            }
+        });
+    }
+
+    private void signOn() {
+        if(auth == null) {
+            auth = FirebaseAuth.getInstance();
+        }
 
     }
 }
