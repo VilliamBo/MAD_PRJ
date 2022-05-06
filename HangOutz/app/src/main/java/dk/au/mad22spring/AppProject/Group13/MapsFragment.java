a9d953b2002c60a569440312e39f64115f1ff5ab
Bering <jonathan.bering@live.dk>
2022-05-06 16:25:50 +0200

    Fixed maps clear() on updates

--- HangOutz/app/src/main/java/dk/au/mad22spring/AppProject/Group13/MapsFragment.java
+++ HangOutz/app/src/main/java/dk/au/mad22spring/AppProject/Group13/MapsFragment.java
@@ -108,7 +108,7 @@ public class MapsFragment extends Fragment {
 
         // Delete all markers = No dupes & old markers.
 
-        //deleteMapMarkers(); // THIS MAKES THE APP CRASH...
+        deleteMapMarkers();
 
         // Add all the updated markers.
         if(viewModel.getFriendLocations() != null){
@@ -140,6 +140,9 @@ public class MapsFragment extends Fragment {
 
     private void deleteMapMarkers() {
         // Deletes all map markers, etc.
-        map.clear();
+        if(map != null)
+        {
+            map.clear();
+        }
     }
 }
